051410f347fb14e0b10c31d28a5fdc6637628279
mudzikalfahri <alfahrimudzik@gmail.com>
2022-02-07 21:12:24 +0700

    feat:add nevbar icon

---
+++ components/HeaderIcon.tsx
@@ -0,0 +1,7 @@
+const HeaderIcon = ({ icon }) => (
+  <div className="icon w-10 h-10 mr-2 rounded-full bg-gray-100 flex items-center justify-center">
+    {icon}
+  </div>
+);
+
+export default HeaderIcon;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import HeaderIcon from "@components/HeaderIcon";
+
 export default function Home() {
   return (
     <div className="bg-white text-gray-800">
-      <header className="fixed top-0 left-0 py-3 w-full shadow-md shadow-gray-500/10">
+      <header className="fixed top-0 left-0 py-3 w-full shadow-md shadow-gray-400/10">
         <div className="max-w-6xl mx-auto px-4 flex items-center justify-between">
           <div className="logo flex items-center space-x-1">
             <svg
@@ -19,6 +21,65 @@ export default function Home() {
               />
             </svg>
             <h1 className="font-bold text-3xl font-serif">Medio</h1>
+          </div>
+          <div className="right-side flex items-center">
+            <HeaderIcon
+              icon={
+                <svg
+                  className="w-5 h-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              }
+            />
+            <HeaderIcon
+              icon={
+                <svg
+                  className="w-5 h-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+                  />
+                </svg>
+              }
+            />
+            <HeaderIcon
+              icon={
+                <svg
+                  className="w-5 h-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                  />
+                </svg>
+              }
+            />
+            <div className="py-1.5 px-4 ml-2 bg-gray-800 rounded-md text-white">
+              Login
+            </div>
           </div>
         </div>
       </header>
